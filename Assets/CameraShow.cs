833a561544445025861a7fb6cbd0d76a8aec85ca
王瀚宇 <huw12313212@gmail.com>
2014-10-25 20:30:57 +0800

    Screen Camera

---
+++ Assets/CameraShow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShow : MonoBehaviour {
+
+	public Vector2 resolution;
+	public Camera screenCamera;
+
+	private RenderTexture texture;
+	private Material material;
+
+	// Use this for initialization
+	void Start () {
+		material = renderer.material;
+		texture = new RenderTexture ((int)resolution.x, (int)resolution.y,0);
+		material.mainTexture = texture;
+		screenCamera.targetTexture = texture;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
